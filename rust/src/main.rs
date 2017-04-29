4f9c1f85207d025d612589934ffe6d2258a7f3cb
Azim Khakulov <ak@netsyno.com>
2017-04-29 23:00:37 +0200

    for loop test

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,13 +2,26 @@ fn main() {
     println!("{}", calculate(vec![1,2,3]));
 }
 
+// fn findBestWeekWindow(vec: Vec<i32>, i: i32) -> usize {
+// 
+//     return -1;
+// }
+
 fn calculate(vec: Vec<i32>) -> usize {
     let current_sum = vec.len() * 2;
-
+    
     if vec.len() < 4 {
         return current_sum;
     }
-
+    
+    // for (i = 0; i < vec.len(); i++) {
+    //     let j: i32 = findBestWeekWindow(A, i);
+    //     print("{}", j);
+    //     // if j != -1 {
+    //     //     const new_prise = calculate(vec[0..i] + vec[j..vec.len()])
+    //     // }
+    // }
+    // 
     current_sum
 }
 
@@ -35,4 +48,14 @@ fn return_7_for_list_with_days_which_fit_into_one_week() {
 #[test]
 fn return_9_for_list_with_days_which_fit_into_one_week_and_extra_day() {
     assert_eq!(calculate(vec![1,2,5,7]), 7);
+}
+
+
+#[test]
+fn for_loop() {
+    let v = vec!["1", "2", "3"];
+    for x in v {
+        println!("{}", x);
+    }
+    assert_eq!(0, 1);
 }
