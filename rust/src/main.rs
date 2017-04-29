f705c7631a43bcafcc7f0c0dc2d1f6a8c5bce012
Azim Khakulov <ak@netsyno.com>
2017-04-29 22:39:43 +0200

    calculate ticket prise for days

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", calculate(vec![1,2,3]));
+}
+
+fn calculate(vec: Vec<i32>) -> usize {
+    let current_sum = vec.len() * 2;
+
+    if vec.len() < 4 {
+        return current_sum;
+    }
+
+    current_sum
+}
+
+#[test]
+fn return_0_for_empty_list() {
+    assert_eq!(calculate(vec![]), 0);
+}
+
+#[test]
+fn return_2_for_list_with_one_element() {
+    assert_eq!(calculate(vec![1]), 2);
+}
+
+#[test]
+fn return_6_for_list_with_three_element() {
+    assert_eq!(calculate(vec![1,2,3]), 6);
+}
+
+#[test]
+fn return_7_for_list_with_days_which_fit_into_one_week() {
+    assert_eq!(calculate(vec![1,2,3,4]), 7);
+}
+
+#[test]
+fn return_9_for_list_with_days_which_fit_into_one_week_and_extra_day() {
+    assert_eq!(calculate(vec![1,2,5,7]), 7);
 }
